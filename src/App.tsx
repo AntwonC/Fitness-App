682c571f5201bab03d386196966c3adf64d6b6c0
Anthony Chen <anthonychen33@gmail.com>
2024-12-14 00:05:14 -0500

    Started on template

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Draggable from 'react-draggable'
+import {DraggableCore} from 'react-draggable'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid-container">
+        <div className="title-a">Title 1</div>
+        <div className="title-b">Title 2</div>
+        <div className="title-c">Title 3</div>
+        <div className="title-d">Title 4</div>
+        
+        
+        <div className="item-b">Temp 3</div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <button className="add-item-button">Add to Grid</button>
     </>
   )
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <h1>Card Placeholder Name</h1>
+        </div>
+    )
+}
+
+export default Card;
