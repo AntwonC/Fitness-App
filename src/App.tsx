08813b24b0cf62f6ecea9f3f76b79fadeba9fa8b
Anthony Chen <anthonychen33@gmail.com>
2024-12-16 10:36:30 -0500

    using different library

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,76 @@
-import { useState } from 'react'
-import Draggable from 'react-draggable'
-import {DraggableCore} from 'react-draggable'
+
+import { useCallback, useState } from 'react'
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd'
 import './App.css'
 
 function App() {
-  
+  const [testData, setTestData] = useState(["item 0", "item 1", "item 2", "item 3"]);
 
-  return (
-    <>
-      <div className="grid-container">
-        <div className="title-a">Title 1</div>
-        <div className="title-b">Title 2</div>
-        <div className="title-c">Title 3</div>
-        <div className="title-d">Title 4</div>
-        
-        
-        <div className="item-b">Temp 3</div>
-        
-      </div>
+  const onDragStart = useCallback(() => {
+    console.log("Starting drag...");
+  }, []);
 
-      <button className="add-item-button">Add to Grid</button>
-    </>
+  const onDragEnd = useCallback(() => {
+    // only one that is required
+    console.log("Something is being dragged...");
+  }, []);
+
+  const showTestList = () => {
+    let count = 0;
+    return (testData.map((element : string) => (
+      <Draggable draggableId={(count++).toFixed()} index={count} >
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <h1>{element}</h1>
+          </div>
+        )}
+      </Draggable>
+    )))
+  }
+
+  const renderItemList = () => {
+    let count = 0;
+    let reverseCount = 99;
+    return (
+      <Droppable droppableId={(reverseCount--).toFixed()}>
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {showTestList()}
+              {provided.placeholder}
+            </div>
+          )}
+      </Droppable>
+    )
+  }
+
+  return (  
+      <DragDropContext
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd} 
+      >
+
+        {renderItemList()}
+        
+      </DragDropContext>
+    
   )
 }
 
 export default App
+
+
+
+     /*   {testData.map((item) => {
+          return <Draggable 
+            key={count++} 
+            draggableId={count.toFixed()} 
+            index={count}
+            children={item}
+            >
+
+            </Draggable> 
+        })} */
