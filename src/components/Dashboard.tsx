27b46ee51f7cd819329e26997a98df1e30f38fd9
Anthony Chen <anthonychen33@gmail.com>
2025-01-21 18:10:48 -0500

    Working on register and login with Ant UI

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button } from 'antd';
+import { Button, Col, Row, Input } from 'antd';
 
 
 
@@ -11,16 +11,26 @@ const Dashboard = () => {
     return (
         
         <div className="square">
-            This is Dashboard
-            <div>An Input goes Here</div>
-            <div>Another Input goes Here</div>
-            <div>Another Input goes here</div>
-           <div className="button-container">
-            <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Register</Button>
-            <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Login</Button>
-           </div>
+            <Row gutter={[16, 8]} justify="center" align="middle">
+                <Col span={24} offset={12}><Input size="small" placeholder="Username" style={ {width: '10em'}}/></Col>
+                <Col span={24} offset={12}><Input.Password size="small" placeholder="Password" style={ { width: '10em' }} /></Col>
+               
+
+                <Col>
+                    <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Register</Button>
+                    <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Login</Button>
+                </Col>
+            </Row>
+
+
+
         </div>  
     )
 }
 
 export default Dashboard;
+
+/*           <div className="button-container">
+            <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Register</Button>
+            <Button variant="outlined" color="default" size="small"  style={{ width: '7em'}}>Login</Button>
+           </div> */ 
