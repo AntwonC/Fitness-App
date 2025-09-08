19ab3c2a8e04c8fea5f8719ccacf96a7762995a6
Anthony Chen <anthonychen33@gmail.com>
2025-09-07 20:34:46 -0400

    Working on UI

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,6 +3,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 import  LogoutButton from './LogoutButton';
 import { Space, Table, Tag } from 'antd';
 import type { TableProps } from 'antd';
+import type { DatePickerProps, TimePickerProps } from 'antd';
+import { DatePicker, Select, TimePicker } from 'antd';
+import type { Dayjs } from 'dayjs';
+import { Button } from 'antd';
+import App from '../App';
 
 import '../styles/Profile.css';
 
@@ -12,6 +17,27 @@ interface DataType {
     age: number; 
     tags: string[];
 }
+
+const { Option } = Select;
+
+type PickerType = 'time' | 'date';
+
+const PickerWithType = ({
+  type,
+  onChange,
+}: {
+  type: PickerType;
+  onChange: TimePickerProps['onChange'] | DatePickerProps['onChange'];
+}) => {
+  if (type === 'time') return <TimePicker onChange={onChange} />;
+  if (type === 'date') return <DatePicker onChange={onChange} />;
+  return <DatePicker picker={type} onChange={onChange} />;
+};
+
+const onChange: DatePickerProps<Dayjs[]>['onChange'] = (date, dateString) => {
+  console.log(date, dateString);
+};
+
 
 const columns: TableProps<DataType>['columns'] = [
     {
@@ -80,10 +106,21 @@ const columns: TableProps<DataType>['columns'] = [
 
 const Profile = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
+    const [addButtonState, setAddButtonState] = useState(false);
+
+    const [type, setType] = useState<PickerType>('time');
+
 
     console.log(user); 
     console.log(isAuthenticated);
     console.log(isLoading);
+
+    const addButtonClicked = () => {
+      console.log("Add button clicked!");
+      console.log("This should prompt a popup box to put information and add.");
+      setAddButtonState(prevState => !prevState);
+      console.log(`addButtonState: ${addButtonState}`);
+    }
 
     if(user === undefined) {
         console.log("user is undefined");
@@ -103,8 +140,25 @@ const Profile = () => {
                 </div>
 
                 <div className="MainMenu">
+                <Space>
+                  <Select aria-label="Picker Type" value={type} onChange={setType}>
+                    <Option value="time">Time</Option>
+                    <Option value="date">Date</Option>
+                    <Option value="week">Week</Option>
+                    <Option value="month">Month</Option>
+                    <Option value="quarter">Quarter</Option>
+                    <Option value="year">Year</Option>
+                  </Select>
+                  <PickerWithType type={type} onChange={(date, dateString) => console.log(dateString)} />
+                </Space>
                     <Table<DataType> scroll={{x: 'max-content'}} columns={columns} dataSource={data} />
+                    <Button onClick={addButtonClicked}>Add</Button>
                 </div>
+
+                  {(addButtonState ? <div><App /></div> : <div></div>)}
+                
+                
+                
                     <LogoutButton />
             </div>
         )
